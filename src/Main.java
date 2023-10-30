b2cbdafef5cfea8bc7016f98eafbeb04d32a6891
Ereb-999 <Naofumi999@yandex.ru>
2023-10-30 09:13:36 +0400

    chapter 1

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,10 @@
 public class Main {
+    public static  int squaring(int i){
+        return i * i;
+    }
 
+    public static void main(String[] args){
+        System.out.println(squaring(15));
+
+    }
 }
